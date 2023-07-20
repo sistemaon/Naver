02b58a9f557b5fb101f736605cbc28e4d99de82b
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-07-20 10:27:54 +0100

    naver spj add middlware jwt

--- env.example.js
+++ env.example.js
@@ -3,5 +3,6 @@ module.exports = {
     PORT: '3000',
     DB_IP: 'localhost',
     DB_PORT: '27017',
-    DB_NAME: 'naver-spj'
+    DB_NAME: 'naver-spj',
+    JWT_SECRET: 'el-secret-del-i-state'
 };
---
+++ middlewares/JsonWebToken/controller/jwt.js
@@ -0,0 +1,44 @@
+
+const jwt = require('jsonwebtoken')
+
+const secret = process.env.JWT_SECRET;
+
+// function to sign user token
+const signJwt = (payload, expires = '1h') => {
+
+    const token = jwt.sign(payload, secret, { expiresIn: expires });
+
+    return token;
+};
+
+// middleware to verify user token
+const verifyJwt = () => (req, res, next) => {
+
+    const token = req.headers['authorization'];
+
+    if (token) {
+
+        const bearer = token.split(' ');
+
+        const bearerToken = bearer[1];
+
+        jwt.verify(bearerToken, secret, (error, decoded) => {
+            if (error) {
+                return res.status(403).json({ success: false, message: error.message });
+            } else {
+                req.decoded = decoded;
+                next(); // goes to 'next' accessible router...
+            }
+        })
+
+    } else {
+        return res.status(403).json({ success: false, message: 'Nonauthentic token!' });
+    }
+}
+
+const objectModuleToExports = {
+    signJwt,
+    verifyJwt
+};
+
+module.exports = objectModuleToExports;
